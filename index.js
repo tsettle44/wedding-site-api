33930a6a76f3dcd8a381cff1d4d2ded05b1899c2
Tom Settle <tsettle44@gmail.com>
2021-02-09 14:31:21 -0600

    connect to mongo

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express()
+const mongoose = require("mongoose");
+const mongodb = require('mongodb').MongoClient
 const port = 3000
+
+const Cred = require('./cred')
+
+mongoose.connect( Cred, { useUnifiedTopology: true, useNewUrlParser: true })
+const db = mongoose.connection
+
+//connection error
+db.on('error', console.error.bind(console, 'connection error'))
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/rsvp', (req, res) => {
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
