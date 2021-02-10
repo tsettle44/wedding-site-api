e51522f7919e7147a5b6e7b839028362361b7e94
Tom Settle <tsettle44@gmail.com>
2021-02-09 19:03:43 -0600

    get with params

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express()
+
+//Middleware
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// parse application/json
+
+
+
 const mongoose = require("mongoose");
 const port = 3000
 //rsvp Model
@@ -15,10 +24,6 @@ const db = mongoose.connection
 //connection error
 db.on('error', console.error.bind(console, 'connection error'))
 
-//Middleware
-app.use(bodyParser.json());
-app.use(cors());
-
 app.post('/rsvp', (req, res) => {
     //new rsvp
     Rsvp.findOneAndUpdate({_id: req.body._id}, { attending: req.body.attending, meal: req.body.meal }, { useFindAndModify: false }, err => {
@@ -27,11 +32,12 @@ app.post('/rsvp', (req, res) => {
     })
 })
 
-app.get('/rsvp', (req, res) => {
+app.get('/rsvp/:firstName/:lastName', (req, res) => {
 
-  Rsvp.find({firstName: req.body.firstName, lastName: req.body.lastName}, (err, guest) => {
+    console.log(req.params)
+  Rsvp.find({firstName: req.params.firstName, lastName: req.params.lastName}, (err, guest) => {
     if (guest.length === 0) {
-        res.send("Name not found")
+        res.send(guest)
         return
     }
       Rsvp.find({party: guest[0].party}, (err,party) => {
